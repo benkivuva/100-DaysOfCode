66e8002a5c0f5c2832f88a6c5787362c1bf18615
Benkivuva <benkivuvak5@gmail.com>
2023-06-02 19:46:39 +0300

    changed

---
+++ Day2/banking_sytstem.py
@@ -0,0 +1,71 @@
+class Bank:
+    """A class representing a bank."""
+    
+    def __init__(self, name):
+        """Initialize the bank with a name and an empty list of customers."""
+        self.name = name
+        self.customers = []
+    
+    def add_customer(self, customer):
+        """Add a customer to the bank's list of customers."""
+        self.customers.append(customer)
+    
+    def remove_customer(self, customer):
+        """Remove a customer from the bank's list of customers."""
+        self.customers.remove(customer)
+    
+    def display_customers(self):
+        """Display the list of customers in the bank."""
+        if not self.customers:
+            print("No customers in the bank.")
+        else:
+            print("Customers in the bank:")
+            for customer in self.customers:
+                print(customer)
+                print("---------")
+    
+class Customer:
+    """A class representing a bank customer."""
+    
+    def __init__(self, name, account_number, account_balance=0):
+        """Initialize the customer with a name, account number, and account balance."""
+        self.name = name
+        self.account_number = account_number
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+        """Deposit money into the customer's account."""
+        self.account_balance += amount
+        print(f"Deposited {amount}$ into the account. New balance: {self.account_balance}$")
+    
+    def withdraw(self, amount):
+        """Withdraw money from the customer's account."""
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            print(f"Withdrew {amount}$ from the account. New balance: {self.account_balance}$")
+        else:
+            print("Insufficient funds.")
+    
+    def display_balance(self):
+        """Display the customer's account balance."""
+        print(f"Account balance: {self.account_balance}$")
+    
+    def __str__(self):
+        """Return a string representation of the customer."""
+        return f"Name: {self.name}\nAccount Number: {self.account_number}\nAccount Balance: {self.account_balance}$"
+
+class Transaction:
+    """A class representing a bank transaction."""
+    
+    def __init__(self, transaction_type, amount, customer):
+        """Initialize the transaction with a type, amount, and customer."""
+        self.transaction_type = transaction_type
+        self.amount = amount
+        self.customer = customer
+    
+    def process_transaction(self):
+        """Process the transaction and update the customer's account balance."""
+        if self.transaction_type == "deposit":
+            self.customer.deposit(self.amount)
+        elif self.transaction_type == "withdrawal":
+            self.customer.withdraw(self.amount)
--- Day2/exampleUsage.py
+++ Day2/exampleUsage.py
@@ -1,70 +1,31 @@
-class Bank:
-    """A class representing a bank."""
-    
-    def __init__(self, name):
-        """Initialize the bank with a name and an empty list of customers."""
-        self.name = name
-        self.customers = []
-    
-    def add_customer(self, customer):
-        """Add a customer to the bank's list of customers."""
-        self.customers.append(customer)
-    
-    def remove_customer(self, customer):
-        """Remove a customer from the bank's list of customers."""
-        self.customers.remove(customer)
-    
-    def display_customers(self):
-        """Display the list of customers in the bank."""
-        if not self.customers:
-            print("No customers in the bank.")
-        else:
-            print("Customers in the bank:")
-            for customer in self.customers:
-                print(customer)
-                print("---------")
-class Customer:
-    """A class representing a bank customer."""
-    
-    def __init__(self, name, account_number, account_balance=0):
-        """Initialize the customer with a name, account number, and account balance."""
-        self.name = name
-        self.account_number = account_number
-        self.account_balance = account_balance
-    
-    def deposit(self, amount):
-        """Deposit money into the customer's account."""
-        self.account_balance += amount
-        print(f"Deposited {amount}$ into the account. New balance: {self.account_balance}$")
-    
-    def withdraw(self, amount):
-        """Withdraw money from the customer's account."""
-        if self.account_balance >= amount:
-            self.account_balance -= amount
-            print(f"Withdrew {amount}$ from the account. New balance: {self.account_balance}$")
-        else:
-            print("Insufficient funds.")
-    
-    def display_balance(self):
-        """Display the customer's account balance."""
-        print(f"Account balance: {self.account_balance}$")
-    
-    def __str__(self):
-        """Return a string representation of the customer."""
-        return f"Name: {self.name}\nAccount Number: {self.account_number}\nAccount Balance: {self.account_balance}$"
-class Transaction:
-    """A class representing a bank transaction."""
-    
-    def __init__(self, transaction_type, amount, customer):
-        """Initialize the transaction with a type, amount, and customer."""
-        self.transaction_type = transaction_type
-        self.amount = amount
-        self.customer = customer
-    
-    def process_transaction(self):
-        """Process the transaction and update the customer's account balance."""
-        if self.transaction_type == "deposit":
-            self.customer.deposit(self.amount)
-        elif self.transaction_type == "withdrawal":
-            self.customer.withdraw(self.amount)
-            
+from banking_system import Bank, Customer, Transaction
+
+# Creating a bank
+bank = Bank("My Bank")
+
+# Adding customers
+customer1 = Customer("Benson", "12345678", 1000)
+customer2 = Customer("Jane", "87654321", 500)
+bank.add_customer(customer1)
+bank.add_customer(customer2)
+
+# Displaying customers
+bank.display_customers()
+
+# Depositing money
+transaction1 = Transaction("deposit", 200, customer1)
+transaction1.process_transaction()
+
+# Withdrawing money
+transaction2 = Transaction("withdrawal", 300, customer2)
+transaction2.process_transaction()
+
+# Displaying account balance
+customer1.display_balance()
+customer2.display_balance()
+
+# Removing a customer
+bank.remove_customer(customer1)
+
+# Displaying customers after removal
+bank.display_customers()
